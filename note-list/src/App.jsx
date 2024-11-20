af815db6d8f9a1ff03c30a61b7e1254e401e4d72
mifnaufal <naufalhhdd@gmail.com>
2024-11-20 08:51:32 +0700

    pertama

---
+++ note-list/src/App.jsx
@@ -0,0 +1,12 @@
+import Post from './components/post'
+
+function App() {
+  return (
+    <div>
+      <Post author="Naufal" body="React is awesome"/>
+      <Post author="Hadi" body="I Like To Learn  Backend"/>
+    </div>
+  )
+}
+
+export default App;
